4f9868e3f0e56c1449f7feb766be7a4550cd6682
Ihar Ratner <46851104+ig0r0k@users.noreply.github.com>
2019-01-28 10:49:58 +0300

    Update bootstrap_tomcat1.sh

--- task3/bootstrap_tomcat1.sh
+++ task3/bootstrap_tomcat1.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
-function isinstalled {
-  if yum list installed "$@" >/dev/null 2>&1; then
-    true
-  else
-    false
-  fi
-}
 
-if isinstalled tomcat $$ tomcat-webapps $$ tomcat-admin-webapps;
-then echo "TOMCAT TOOLS INSTALLED"
-else
-echo "START INSTALL TOMCAT TOOLS"
 yum install tomcat tomcat-webapps tomcat-admin-webapps -y
 systemctl enable tomcat
 systemctl start tomcat
-fi
 
 if [ -d /usr/share/tomcat/webapps/test ]; then
 echo "DIRECTORY TEST IS ALREADY EXISTS"
