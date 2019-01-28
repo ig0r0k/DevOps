67c9880f82484880d353d0e6b994befd094b1bc7
Ihar Ratner <46851104+ig0r0k@users.noreply.github.com>
2019-01-28 12:06:09 +0300

    Update bootstrap_server1.sh

--- task2/bootstrap_server1.sh
+++ task2/bootstrap_server1.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-if grep "192.168.33.11 server2" /etc/hosts;
-then
-echo "The string is already exists"
+if grep "192.168.33.11 server2" /etc/hosts; then
+  echo "The string is already exists"
 else
-echo "192.168.33.11 server2" >> /etc/hosts
+  echo "192.168.33.11 server2" >> /etc/hosts
 fi
 
 yum install git -y
 
 if [ -d /home/vagrant/DevOps ]; then
-echo "DIRECTORY DevOps IS ALREADY EXIST"
+  echo "DIRECTORY DevOps IS ALREADY EXIST"
 else
-echo "GIT CLONE + CAT TEST.TXT"
-git clone https://github.com/ig0r0k/DevOps.git
-cd ./DevOps/
-git checkout task2
-cat ./task2/test.txt
+  echo "GIT CLONE + CAT TEST.TXT"
+  git clone https://github.com/ig0r0k/DevOps.git
+  cd ./DevOps/
+  git checkout task2
+  cat ./task2/test.txt
 fi
