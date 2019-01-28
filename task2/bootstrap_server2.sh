0f7cc91dd410d6cb04a9724b65c71798a2af91b4
Ihar Ratner <46851104+ig0r0k@users.noreply.github.com>
2019-01-28 12:06:43 +0300

    Update bootstrap_server2.sh

--- task2/bootstrap_server2.sh
+++ task2/bootstrap_server2.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-if grep "192.168.33.10 server1" /etc/hosts;
-then
-echo "The string is already exists"
+if grep "192.168.33.10 server1" /etc/hosts; then
+  echo "The string is already exists"
 else
-echo "192.168.33.10 server1" >> /etc/hosts
+  echo "192.168.33.10 server1" >> /etc/hosts
 fi
