9441aad8035e9e4ca508fed1ff1ba118dba1268f
Ihar Ratner <46851104+ig0r0k@users.noreply.github.com>
2019-01-28 10:51:33 +0300

    Update bootstrap_balancer.sh

--- task3/bootstrap_balancer.sh
+++ task3/bootstrap_balancer.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env bash
-function isinstalled {
-  if yum list installed "$@" >/dev/null 2>&1; then
-    true
-  else
-    false
-  fi
-}
 
-if isinstalled httpd;
-then echo "HTTPD INSTALLED"
-else
-echo "START INSTALL HTTPD"
 yum install httpd -y
 systemctl enable httpd
-fi
 
 if [ -f /etc/httpd/modules/mod_jk.so ]; then
 echo "MOD_JK IS ALREADY EXISTS"
@@ -38,10 +26,3 @@ cat /var/www/config >> /etc/httpd/conf/httpd.conf
 fi
 
 systemctl start httpd
-
-#yum install httpd -y
-#cp /var/www/mod_jk.so /etc/httpd/modules/
-#cp /var/www/workers.properties /etc/httpd/conf
-#cat /var/www/config >> /etc/httpd/conf/httpd.conf
-
-
