69ef743236848ec1eb7469363c4b8c0bf6e8ee24
Ihar Ratner <46851104+ig0r0k@users.noreply.github.com>
2019-01-28 11:58:48 +0300

    Update bootstrap_balancer.sh

--- task3/bootstrap_balancer.sh
+++ task3/bootstrap_balancer.sh
@@ -4,25 +4,23 @@ yum install httpd -y
 systemctl enable httpd
 
 if [ -f /etc/httpd/modules/mod_jk.so ]; then
-echo "MOD_JK IS ALREADY EXISTS"
+  echo "MOD_JK IS ALREADY EXISTS"
 else
-echo "TRANSPORT MOD_JK"
-cp /var/www/mod_jk.so /etc/httpd/modules/
+  echo "TRANSPORT MOD_JK"
+  cp /var/www/mod_jk.so /etc/httpd/modules/
 fi
 
-if [ -f /etc/httpd/conf/workers.properties ];
-then
-echo "WORKERS_PROPERTIES IS ALREADY EXISTS"
+if [ -f /etc/httpd/conf/workers.properties ]; then
+  echo "WORKERS_PROPERTIES IS ALREADY EXISTS"
 else
-echo "TRANSPORT WORKERS_PROPERTIES"
-cp /var/www/workers.properties /etc/httpd/conf
+  echo "TRANSPORT WORKERS_PROPERTIES"
+  cp /var/www/workers.properties /etc/httpd/conf
 fi
 
-if grep "worker.list=lb" /var/www/config;
-then
-echo "The string is already exists"
+if grep "worker.list=lb" /var/www/config; then
+  echo "The string is already exists"
 else
-cat /var/www/config >> /etc/httpd/conf/httpd.conf
+  cat /var/www/config >> /etc/httpd/conf/httpd.conf
 fi
 
 systemctl start httpd
