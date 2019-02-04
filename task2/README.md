106d4fb504c723dbf5af7cd1e7ab5ccc96415da8
Ihar Ratner <46851104+ig0r0k@users.noreply.github.com>
2019-02-04 19:05:13 +0300

    Add files via upload

---
+++ task2/README.md
@@ -0,0 +1,3 @@
+Vagrantfile - up two VM's, one of them install git and clone my repository
+bootstrap_server1.sh - provision config for server1
+bootstrap_server2.sh - provision config for server2
---
+++ task2/bootstrap_server1.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if grep "192.168.33.11 server2" /etc/hosts; then
+  echo "The string is already exists"
+else
+  echo "192.168.33.11 server2" >> /etc/hosts
+fi
+
+yum install git -y
+
+if [ -d /home/vagrant/DevOps ]; then
+  echo "DIRECTORY DevOps IS ALREADY EXIST"
+else
+  echo "GIT CLONE + CAT TEST.TXT"
+  git clone https://github.com/ig0r0k/DevOps.git
+  cd ./DevOps/
+  git checkout task2
+  cat ./task2/test.txt
+fi
