85b389a8d1a9e8e09aa36b6afe608e04bdc82121
Ihar Ratner <46851104+ig0r0k@users.noreply.github.com>
2019-03-19 19:41:05 +0300

    Update default_test.rb

--- mytest/test/integration/default/default_test.rb
+++ mytest/test/integration/default/default_test.rb
@@ -1,18 +1,15 @@
-# # encoding: utf-8
-
-# Inspec test for recipe mytest::default
-
-# The Inspec reference, with examples and extensive documentation, can be
-# found at http://inspec.io/docs/reference/resources/
-
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe service('docker') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe docker_container(name: 'test-reg') do
+  it { should exist }
+  it { should be_running }
+end
+
+describe file('/etc/docker/daemon.json') do
+  it { should exist }
+  its('content') { should match(/insecure-registries/) }
 end
