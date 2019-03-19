0a7738df86f89d987422f5d3077e9f701a4afcdb
Ihar Ratner <46851104+ig0r0k@users.noreply.github.com>
2019-03-19 19:36:51 +0300

    Update default.rb

--- mytest/recipes/default.rb
+++ mytest/recipes/default.rb
@@ -7,15 +7,19 @@ docker_image node['mytest']['image'] do
   action :pull
 end
 
-docker_container 'test-reg' do
-  repo node['mytest']['repo']
-  tag node['mytest']['tag']
-  port node['mytest']['port']
-end
-
 file node['mytest']['file'] do
   action [:create]
   content '{
   "insecure-registries" : ["http://10.186.106.151:5000"]
   }'
+end
+
+docker_service 'default' do
+  action [:restart]
+end
+
+docker_container 'test-reg' do
+  repo node['mytest']['repo']
+  tag node['mytest']['tag']
+  port node['mytest']['port']
 end
