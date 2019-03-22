7fbc2df3d027b647bad84afedc936428ebaa4581
Ihar Ratner <46851104+ig0r0k@users.noreply.github.com>
2019-03-23 02:36:07 +0300

    Update default.rb

--- ci_node/recipes/default.rb
+++ ci_node/recipes/default.rb
@@ -7,7 +7,9 @@
 bash 'install agent' do
   code <<-EOH
     yum install java-1.8.0-openjdk java-1.8.0-openjdk-devel -y
-	wget 10.186.106.155:8000/jnlpJars/agent.jar
-	nohup java -jar agent.jar -jnlpUrl http://10.186.106.155:8000/computer/centos_slave/slave-agent.jnlp -secret c60384f59d7d255905a7df981c4fae0269a23ef6e8c1d62253546162ae40ab63 -workDir "/home/centos_slave" &
-    EOH
+	wget 10.186.106.155:8080/jnlpJars/agent.jar
+	nohup java -jar agent.jar -jnlpUrl http://10.186.106.155:8080/computer/centos_slave/slave-agent.jnlp -secret c60384f59d7d255905a7df981c4fae0269a23ef6e8c1d62253546162ae40ab63 -workDir "/home/centos_slave" &
+        ps aux > /tmp/1.txt
+   EOH
+   not_if 'cat /tmp/1.txt | grep java'
 end
