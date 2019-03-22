5b23ef8a45cdd37335eb672f797701c95764b5d3
Ihar Ratner <46851104+ig0r0k@users.noreply.github.com>
2019-03-22 23:41:15 +0300

    Update default.rb

--- jenkins/attributes/default.rb
+++ jenkins/attributes/default.rb
@@ -4,4 +4,4 @@ default['jenkins']['image'] = 'jenkins/jenkins'
 default['jenkins']['tag'] = 'lts-alpine'
 default['jenkins']['port'] = ['8000:8080', '50000:50000']
 default['jenkins']['repo'] = 'jenkins/jenkins'
-default['jenkins']['volume'] = '/etc/jenkins_home:/home'
+default['jenkins']['volume'] = '/etc/jenkins_home:/var/jenkins_home'
