215b8c57093c0e75d316632d6242e997d9a40206
Ihar Ratner <46851104+ig0r0k@users.noreply.github.com>
2019-03-23 12:31:14 +0300

    Delete default.rb

--- jenkins/attributes/default.rb
+++
@@ -1,7 +0,0 @@
-default['jenkins']['username'] = 'jenkins'
-default['jenkins']['shell'] = '/bin/bash/'
-default['jenkins']['image'] = 'jenkins/jenkins'
-default['jenkins']['tag'] = 'lts-alpine'
-default['jenkins']['port'] = ['8000:8080', '50000:50000']
-default['jenkins']['repo'] = 'jenkins/jenkins'
-default['jenkins']['volume'] = '/etc/jenkins_home:/home'
