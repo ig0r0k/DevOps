9e9eb983fe75d80800b780f395f781934d22e4b4
Ihar Ratner <46851104+ig0r0k@users.noreply.github.com>
2019-03-27 13:19:55 +0300

    Update ENV.groovy

--- ENV.groovy
+++ ENV.groovy
@@ -4,4 +4,5 @@ def cmd = "knife environment list -F json -c /home/igor/chef-repo/.chef/knife.rb
 Process process = cmd.execute()
 process.waitFor()
 def envlist = new JsonSlurper().parseText(process.text)
+
 return envlist.sort()
