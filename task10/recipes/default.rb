a60dbc7bb895b89e3c88b890fe68dd83854588eb
Ihar Ratner <46851104+ig0r0k@users.noreply.github.com>
2019-03-29 15:08:38 +0300

    Add files via upload

--- task10/recipes/default.rb
+++ task10/recipes/default.rb
@@ -22,19 +22,32 @@ docker_image node['task10']['image'] do
   action :pull
 end
 
-######remove blue######
-execute 'blue' do
-  command 'docker stop task10_blue && docker rm task10_blue'
-  action [:nothing]
-end
-
 ######green########
 check_green = docker_container 'task10_green' do
   repo node['task10']['repo']
   tag node['task10']['tag']
   port node['task10']['port_green']
-  notifies :run, 'execute[blue]', :immediately
+  #notifies :run, 'execute[blue]', :immediately
   only_if 'docker ps | grep 8080'
+end
+
+#####check_content_green#####
+ruby_block "check_green" do
+        block do
+            require 'chef/mixin/shell_out'
+            Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
+            sleep(20)
+            command = 'curl http://localhost:8081/test/'
+            command_out = shell_out(command)
+            raise "INCORRECT DEPLOY" if !command_out.stdout.include? node['task10']['tag']
+        end
+	only_if { check_green.updated_by_last_action? }
+end
+
+######remove blue######
+execute 'blue' do
+  command 'docker stop task10_blue && docker rm task10_blue'
+  only_if { check_green.updated_by_last_action? }
 end
 
 #####blue#####
@@ -42,9 +55,21 @@ check_blue = docker_container 'task10_blue' do
   repo node['task10']['repo']
   tag node['task10']['tag']
   port node['task10']['port']
-  #notifies :run, 'execute[green]', :immediately
   only_if 'docker ps | grep 8081'
   not_if { check_green.updated_by_last_action? }
+end
+
+#####check_content_blue#####
+ruby_block "check_blue" do
+        block do
+            require 'chef/mixin/shell_out'
+            Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
+            sleep(20)
+            command = 'curl http://localhost:8080/test/'
+            command_out = shell_out(command)
+            raise "INCORRECT DEPLOY" if !command_out.stdout.include? node['task10']['tag']
+        end
+	only_if { check_blue.updated_by_last_action? }
 end
 
 #####remove green#####
