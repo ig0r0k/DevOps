ddbd1487a18f1b6e824d8909b51806e24a4bb3a7
Ihar Ratner <46851104+ig0r0k@users.noreply.github.com>
2019-03-31 13:12:34 +0300

    Update template.tf

--- template.tf
+++ template.tf
@@ -1,5 +1,4 @@
-# Create a new instance of the latest Ubuntu 14.04 on an
-# t2.micro node with an AWS Tag naming it "HelloWorld"
+# Create a new instance of the latest Ubuntu 14.04
 
 provider "aws" {
   region = "us-east-2"
